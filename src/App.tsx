006d2f1bff76dd9070e18559d515568c2ffd4e67
mehul4real <mehulsaxena146@gmail.com>
2025-10-18 13:28:56 +0530

    router handling

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/portfolioV2">
           <Routes>
             <Route path="/" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
